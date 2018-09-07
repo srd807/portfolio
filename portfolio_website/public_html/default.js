18765eac366bfd15aa66ab4aeb05ac4c99d4fe03
Simone <srd807@mail.usask.ca>
2018-09-07 16:34:20 -0600

    added lightboxes to every picture on the project pages with caption

--- portfolio_website/public_html/default.js
+++ portfolio_website/public_html/default.js
@@ -3,3 +3,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var demoTrigger = document.querySelector('.demo-trigger');
+new Luminous(demoTrigger);
